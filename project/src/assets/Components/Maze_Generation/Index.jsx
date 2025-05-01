5074bdde99cb6f6de8569a38b3bd8766501b7583
Shubham <shubhamyadav45623@gmail.com>
2025-05-01 16:17:30 +0530

    coloring component in maze generation

--- project/src/assets/Components/Maze_Generation/Index.jsx
+++ project/src/assets/Components/Maze_Generation/Index.jsx
@@ -12,6 +12,10 @@ export default function Index() {
   const [currentStep, setCurrentStep] = useState(0)
   const [progress, setProgress] = useState(0)
   const [simulationSpeed, setSimulationSpeed] = useState(200)
+  const [highlightedEdge, setHighlightedEdge] = useState(null);
+  const [componentColors, setComponentColors] = useState({})
+  const [breakingWall, setBreakingWall] = useState(null)
+  const [isBreaking, setIsBreaking] = useState(false)
   
   const mazeRef = useRef()
 
@@ -110,6 +114,12 @@ export default function Index() {
     }))
   }
 
+  //get random colour for each component
+  const generatePastelColor = () => {
+    const hue = Math.floor(Math.random() * 360)
+    return `hsl(${hue}, 70%, 80%)`
+  }
+
   // useEffect(() => {
   //   generateMaze();
   // }, [grid]);
@@ -136,6 +146,7 @@ export default function Index() {
     let pp = []
     let rr = []
     let ed = []
+    let initialColors = {};
     for(let i=0; i<dimensions.row; i++){
       let single_row = []
       for(let j=0; j<dimensions.col; j++){
@@ -147,6 +158,8 @@ export default function Index() {
         let idx = get_id(i,j,dimensions.col)
         pp[idx]=idx
         rr[idx]=0
+
+        initialColors[idx] = generatePastelColor()
 
         if (j < dimensions.col - 1) {
           ed.push({
@@ -171,6 +184,7 @@ export default function Index() {
     setRank(rr)
     setEdges(ed)
     // console.log(grid)
+    setComponentColors(initialColors);
   }
 
  
@@ -183,12 +197,14 @@ export default function Index() {
   const union_by_rank = (x,y)=>{
     let p_x = get_parent(x)
     let p_y = get_parent(y)
+    // const newComponentColors = {...componentColors}
     if(p_x == p_y){
       // console.log("same parent")
       return false;
     }
     if(rank[p_x]>rank[p_y]){
       parent[p_y] = p_x
+      // newComponentColors[idx] = colorToKeep
     }
     else if(rank[p_y]>rank[p_x]){
       parent[p_x] = p_y
@@ -216,47 +232,93 @@ export default function Index() {
     
     let n1 = edges[currentStep].node1
     let n2 = edges[currentStep].node2
+
+    setHighlightedEdge({ node1: n1, node2: n2 });
+
     let p1 = get_id(n1[0],n1[1],dimensions.col)
     let p2 = get_id(n2[0],n2[1],dimensions.col)
     const newGrid = [...grid]
     let merged = union_by_rank(p1,p2);
+    
+   
+
+
     if(merged==true){
-      console.log(n1,n2)
-      if(n1[0]==n2[0]){
-        if(n1[1]>n2[1]){
-          console.log("aaa")
-          newGrid[n1[0]][n1[1]].wall.left = 0
-          newGrid[n2[0]][n2[1]].wall.right = 0
+      // console.log(n1,n2)
+
+      let wallInfo = {
+        cell1: { row: n1[0], col: n1[1], side: '' },
+        cell2: { row: n2[0], col: n2[1], side: '' }
+      }
+
+      if(n1[0] === n2[0]) { // Same row - horizontal neighbors
+        if(n1[1] > n2[1]) { // n1 is to the right of n2
+          wallInfo.cell1.side = 'left'
+          wallInfo.cell2.side = 'right'
+        } else {
+          wallInfo.cell1.side = 'right'
+          wallInfo.cell2.side = 'left'
         }
-        else{
-          console.log("bbb")
-          newGrid[n1[0]][n1[1]].wall.right = 0
-          newGrid[n2[0]][n2[1]].wall.left = 0
+      } else { // Same column - vertical neighbors
+        if(n1[0] > n2[0]) { // n1 is below n2
+          wallInfo.cell1.side = 'top'
+          wallInfo.cell2.side = 'bottom'
+        } else {
+          wallInfo.cell1.side = 'bottom'
+          wallInfo.cell2.side = 'top'
         }
       }
-      else{
-        if(n1[0]>n2[0]){
-          console.log("cccc")
-          newGrid[n1[0]][n1[1]].wall.top = 0
-          newGrid[n2[0]][n2[1]].wall.bottom = 0
+
+      setBreakingWall(wallInfo)
+      setIsBreaking(true)
+
+      setTimeout(()=>{
+
+     
+        if(n1[0]==n2[0]){
+          if(n1[1]>n2[1]){
+            // console.log("aaa")
+            newGrid[n1[0]][n1[1]].wall.left = 0
+            newGrid[n2[0]][n2[1]].wall.right = 0
+          }
+          else{
+            // console.log("bbb")
+            newGrid[n1[0]][n1[1]].wall.right = 0
+            newGrid[n2[0]][n2[1]].wall.left = 0
+          }
         }
         else{
-          console.log("dddd")
-          newGrid[n1[0]][n1[1]].wall.bottom = 0
-          newGrid[n2[0]][n2[1]].wall.top = 0
+          if(n1[0]>n2[0]){
+            // console.log("cccc")
+            newGrid[n1[0]][n1[1]].wall.top = 0
+            newGrid[n2[0]][n2[1]].wall.bottom = 0
+          }
+          else{
+            // console.log("dddd")
+            newGrid[n1[0]][n1[1]].wall.bottom = 0
+            newGrid[n2[0]][n2[1]].wall.top = 0
+          }
         }
-      }
-      // cnt+=1
-      setGrid([...newGrid]); // force re-render with new wall
+        // cnt+=1
+        setGrid([...newGrid]); // force re-render with new wall
+        setCurrentStep(currentStep + 1);
+        setProgress(Math.round((currentStep + 1) / edges.length * 100));
+      },simulationSpeed/2)
+      return merged
+    }else{
+      setCurrentStep(currentStep + 1)
+      setProgress(Math.round((currentStep + 1) / edges.length * 100))
+      return merged
     }
-    setCurrentStep(currentStep + 1);
-    setProgress(Math.round((currentStep + 1) / edges.length * 100));
-    return merged
   };
 
   const startSimulation = async()=>{
     if (!isSimulating) {
       setIsSimulating(true);
+      console.log("geenearetd grid")
+      console.log(grid);
+      console.log("genearted edge")
+      console.log(edges);
     }
   }
 
@@ -276,6 +338,7 @@ export default function Index() {
   useEffect(() => {
     if (isSimulating && currentStep < edges.length) {
       const timer = setTimeout(() => {
+        console.log(highlightedEdge)
         next_Merge();
       }, simulationSpeed);
   
@@ -284,6 +347,53 @@ export default function Index() {
       setIsSimulating(false);
     }
   }, [isSimulating, currentStep, simulationSpeed]);
+
+  const getCellColor = (row, col) => {
+    const cellId = get_id(row, col, dimensions.col)
+    const rootParent = get_parent(cellId)
+    return componentColors[rootParent] || '#ffffff'
+  }
+
+  // Function to determine if a wall is being broken
+  const isWallBreaking = (row, col, side) => {
+    if (!breakingWall || !isBreaking) return false;
+    
+    return (
+      (breakingWall.cell1.row === row && 
+       breakingWall.cell1.col === col && 
+       breakingWall.cell1.side === side) ||
+      (breakingWall.cell2.row === row && 
+       breakingWall.cell2.col === col && 
+       breakingWall.cell2.side === side)
+    );
+  };
+
+  // Generate wall styles with animation
+  const getWallStyle = (element, side) => {
+    const isBreakingThisWall = isWallBreaking(element.r, element.c, side);
+    
+    // Base wall visibility
+    const isWallVisible = element.wall[side] === 1;
+    
+    // Don't show animation for walls that aren't there
+    if (!isWallVisible) return '2px solid transparent';
+    
+    // Apply animation to the wall that's breaking
+    if (isBreakingThisWall) {
+      return {
+        borderStyle: 'dashed',
+        borderWidth: '2px',
+        borderColor: 'red',
+        animation: 'wallBreaking 0.5s ease-in-out'
+      };
+    }
+    
+    // Regular wall style
+    return '2px solid black';
+  };
+
+
+
   return (
     <div className="flex flex-col items-center p-4 bg-gray-100 min-h-screen">
       <h1 className="text-2xl font-bold mb-4 text-black">Maze Generator (Kruskal's Algorithm)</h1>
@@ -386,20 +496,31 @@ export default function Index() {
         <div className="playground">
           {grid.map((row, rowIndex) => (
             <div key={rowIndex} className="flex">
-              {row.map((element, colIndex) => (
-                <div key={colIndex}
-                  className="w-16 h-16 flex items-center justify-center text-sm"
-                  style={{
-                    borderTop: element.wall.top ? '2px solid black' : '2px solid transparent',
-                    borderBottom: element.wall.bottom ? '2px solid black' : '2px solid transparent',
-                    borderLeft: element.wall.left ? '2px solid black' : '2px solid transparent',
-                    borderRight: element.wall.right ? '2px solid black' : '2px solid transparent',
-                  }}
-                >
-                  {/* {`${element.r},${element.c}`} */}
-                  <p></p>
-                </div>
-              ))}
+              {row.map((element, colIndex) => {
+                console.log(highlightedEdge)
+                const isHighlighted = highlightedEdge &&
+                ((highlightedEdge.node1[0] === element.r && highlightedEdge.node1[1] === element.c) ||
+                (highlightedEdge.node2[0] === element.r && highlightedEdge.node2[1] === element.c));
+
+                // backgroundColor: isHighlighted ? '#facc15' : 'white'; // yellow highlight
+                const cellColor = getCellColor(element.r, element.c)
+
+                return (
+                  <div key={colIndex}
+                    className="w-16 h-16 flex items-center justify-center text-sm "
+                    style={{
+                      borderTop: element.wall.top ? '2px solid black' : '2px solid transparent',
+                      borderBottom: element.wall.bottom ? '2px solid black' : '2px solid transparent',
+                      borderLeft: element.wall.left ? '2px solid black' : '2px solid transparent',
+                      borderRight: element.wall.right ? '2px solid black' : '2px solid transparent',
+                      backgroundColor: cellColor, // yellow highlight
+                    }}
+                  >
+                    {/* {`${element.r},${element.c}`} */}
+                    <p></p>
+                  </div>
+                )
+              })}
             </div>
           ))}
         </div>
